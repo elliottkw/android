edd612bb47d233a9a04ae5aaa0311edec111c73f
Elliott <resiel@gmail.com>
2020-12-09 20:30:38 -0600

    Intent implicit phone - Part 2.

--- app1-designing-in-aindroid/app/src/main/java/mx/qosoft/designing/ThirdActivity.java
+++ app1-designing-in-aindroid/app/src/main/java/mx/qosoft/designing/ThirdActivity.java
@@ -38,13 +38,15 @@ public class ThirdActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String phoneNumber = editTextPhone.getText().toString();
-                if(phoneNumber != null) {
+                if(phoneNumber != null && !phoneNumber.isEmpty()) {
                     // Check actual version of android with we have execute.
                     if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, PHONE_CALL_CODE);
                     } else {
                         OlderVersions(phoneNumber);
                     }
+                } else {
+                    Toast.makeText(ThirdActivity.this, "Insert a phone number", Toast.LENGTH_SHORT).show();
                 }
             }
 
